06cd0f5380036163c620627e67a2804f87ff980e
togashitoga <wqfqw3200@kjb.biglobe.ne.jp>
2021-12-08 12:20:38 +0900

    ゲームのユーザー名を表現するUserNameを実装
    -文字数に感留守ルールをうまく１箇所に集めることができた
    -’screen_name（）’で管理したので、要件が変わっても変更がラク
    
    *タイトル
    
    詳細
    詳細

--- oop_prictice1.py
+++ oop_prictice1.py
@@ -1,1 +1,31 @@
-#オブジェクト指向の練習を開始します
+# オブジェクト指向の練習を開始します
+'''
+ユーザーネーム:自分独自のデータ型を作る
+データ型宣言：UserName
+　　　　属性：実際のユーザー名
+　　　ルール：ユーザー名は4文字以上20文字以下である
+　できること：ユーザー名を大文字に変換する=>メソッド
+'''
+
+
+class UserName:
+    def __init__(self, name):
+        if not (4 <= len(name) <= 20):
+            raise ValueError(f'{name}は文字数ルール違反やで！')
+        self.name = name
+
+    def screen_name(self):
+        return self.name.upper()
+# raise:は意図的に間違いを起こす（こっちから怒ってやるぜ）為に使う
+
+# UserNameクラスのインスタンス化
+hibiki = UserName(name='hibiki')
+print(hibiki.screen_name())
+# print(hibiki)
+# print(type(hibiki)):typeで型がわかる
+# print(hibiki.name)#.を入れる事でその値にアクセスできる
+
+# sho = UserName('sho')
+# print(sho.name)
+
+# string型で大文字にするには.upper()を使う
